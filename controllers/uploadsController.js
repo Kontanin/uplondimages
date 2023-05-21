28d7dadeea2b9dc150b7cf5471a5a70f2db9bdae
Kontanin <boss12853@gmail.com>
2023-05-21 11:30:53 +0700

    add packe

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,12 +1,24 @@
 const path=require('path')
 const{StatusCodes}=require('http-status-codes');
-
+const CustomError =require('../errors')
 const uploadProductImage = async (req, res) => {
+    if(!req.files){
+        throw new CustomError.BadRequestError('no files')
+    }
     const productImage=req.files.image;
+    if(!productImage.mimetype.startsWith('')){
+        throw new CustomError.BadRequestError('Please upload image')
+    }
+    const maxSize= 1024 
+    if (productImage.size>maxSize){
+        throw new CustomError.BadRequestError('Please upload image semeller')
+
+    }
     const imagePath=path.join(__dirname,'../pubulic/uploads/'+`${productImage.name}`
     );
     await productImage.mv(imagePath);
-    return res.status()
+    return res.status(StatusCodes.OK).json({image:{src:`/upload/
+    ${productImage.name}`}})
 }
 
 
